cfe9f034ad143aa69e728e70f86f3c2cddb41039
Agnes <agypeter97@gmail.com>
2024-10-31 17:43:30 +0300

    define methods for displaying login form +handling login

--- app/Http/Controllers/Auth/CustomerLoginController.php
+++ app/Http/Controllers/Auth/CustomerLoginController.php
@@ -8,4 +8,23 @@ use App\Http\Controllers\Controller;
 class CustomerLoginController extends Controller
 {
     //
+    public function showLoginForm(){
+        return view("auth.customer_login");
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'username'=>'required',
+            'password'=> 'required',
+        ]);
+
+        if(Auth::guard('customer')->attempt($credentials)){
+            return redirect()->intended('/customer/dashboard');
+        }
+
+        return back()->withErrors([
+            'username'=>'The provided credentials do not match our records.',
+        ]);
+}
+
 }
