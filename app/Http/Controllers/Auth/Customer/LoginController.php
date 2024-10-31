da27ce478c661979e417b0dc1ed6524eb947861d
Agnes <agypeter97@gmail.com>
2024-10-31 19:31:35 +0300

    re-write login methods to display customer login form

--- app/Http/Controllers/Auth/Customer/LoginController.php
+++ app/Http/Controllers/Auth/Customer/LoginController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers\Auth\Customer;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = "/customer/dashboard";
+
+    public function __construct(){
+        $this->middleware("customer.guest")->except("logout");
+    }
+
+    public function showLoginForm(){
+        return view("auth.customer.login");
+    }
+
+    protected function guard(){
+        return Auth::guard("customer");
+    }
 }
