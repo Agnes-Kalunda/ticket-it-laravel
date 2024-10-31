4bcc7a6520a09f131a7a20030956c5972ec82e9b
Agnes <agypeter97@gmail.com>
2024-10-31 19:55:45 +0300

    add registration methods to handle new custom user registration

--- app/Http/Controllers/Auth/Customer/RegisterController.php
+++ app/Http/Controllers/Auth/Customer/RegisterController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers\Auth\Customer;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Customer;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     //
+    use RegistersUsers;
+
+    protected $redirectTo = "/customer/dashboard";
+
+    public function __construct(){
+        $this->middleware("customer.guest");
+    }
+
+    public function showRegistrationForm(){
+        return view("auth.customer.register");
 }
+
+
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:customers'],
+            'username' => ['required', 'string', 'max:255', 'unique:customers'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    protected function create(array $data){
+        return Customer::create([
+            'name'=> $data['name'],
+            'email'=> $data['email'],
+            'username'=> $data['username'],
+            'password'=> Hash::make($data['password']),
+            ]);
+        }
+
+    protected function guard(){
+        return Auth::guard('customer');
+    }
+}    
+
