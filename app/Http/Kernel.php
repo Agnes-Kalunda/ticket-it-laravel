1e1bbf8510a2c7a7baa80a9b7d6fc2ebfda0ba59
Agnes <agypeter97@gmail.com>
2024-10-31 19:18:35 +0300

    create middleware for not authenticated customers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'customer.auth' => \App\Http\Middleware\RedirectIfNotCustomer::class,
+        'customer.guest' => \App\Http\Middleware\RedirectIfCustomerAuthenticated::class,
     ];
 
     /**
